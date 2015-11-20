f37d825984e2675006e04cede3b1ef8c323dcc3c
Dan B. Fujita <dan.b.fujita@gmail.com>
2015-11-20 15:58:29 -0500

    thrid commit

---
+++ Hackathon/bookingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  bookingViewController.swift
+//  Hackathon
+//
+//  Created by Dan B. Fujita on 11/20/15.
+//  Copyright © 2015 Dan B. Fujita. All rights reserved.
+//
+
+import Foundation
---
+++ Hackathon/detailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  detailsViewController.swift
+//  Hackathon
+//
+//  Created by Dan B. Fujita on 11/20/15.
+//  Copyright © 2015 Dan B. Fujita. All rights reserved.
+//
+
+import Foundation
